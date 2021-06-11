955815265edd15f60f57c4f1d9bd2e206d75512b
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-11 14:20:41 +0100

    Add files via upload
    Graph with dplyr functionality limiting the x axis

--- experimental blueprint for the challenge of creating a parameter space by 17th June.R
+++ experimental blueprint for the challenge of creating a parameter space by 17th June.R
@@ -252,11 +252,12 @@ par_r<-parLapply(1,fun=r_calculate,cl=cl)
 stopCluster(cl)
 
 mean_r<-mean(unlist(par_r))
-xmaxtemp<-temp%>%filter(temp$infected<999)%>%max(temp$time)
+temptimemax<-temp%>%filter(infected<999)%>%filter(time==max(time))
+temptimemax<-temptimemax[,"time"]
 pred_data <- data.frame(time=times, infected=logis(r=mean_r, t=times, K=1000, q0=1))
-ggplot(temp) + geom_line(aes(x=time, y=infected, group=sim, xmax=xmaxtemp), size=.2) +
+ggplot(temp) + geom_line(aes(x=time, y=infected, group=sim), size=.2) +
   geom_line(data=filter(pred_data, infected<1000), aes(x=time, y=infected), colour="red", size=1)+
-  ggtitle("Epidemic growth curve for 1000 simulations")+theme_classic()
+  ggtitle("Epidemic growth curve for 1000 simulations")+theme_classic()+xlim(0,temptimemax)
 length(unique(unlist(par_r)))
 mean_r
 proc.end2<-proc.time()-t2
