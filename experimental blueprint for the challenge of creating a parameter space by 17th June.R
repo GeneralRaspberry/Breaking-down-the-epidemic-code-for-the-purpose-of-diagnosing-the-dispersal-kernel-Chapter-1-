1b680e952f6e4271a3af4d409dd79f6bf9f56631
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-11 18:39:25 +0100

    Add files via upload
    Using quantile function instead of summary

--- experimental blueprint for the challenge of creating a parameter space by 17th June.R
+++ experimental blueprint for the challenge of creating a parameter space by 17th June.R
@@ -286,9 +286,9 @@ mean_r
 proc.end2<-proc.time()-t2
 
 ################################creating a time graph for this simulation###################################
-
-timestampdata<-data%>%group_by(x)%>%do(data.frame(time=summary(times)[2:4],
-                                                  infected=sapply(summary(times[2:4]),function(x) sum(.$time<= x))))
+timestest<-quantile(times,probs = .25,5,.75)
+timestampdata<-data%>%group_by(x)%>%do(data.frame(time=timestest[2:4],
+                                                  infected=sapply(timestest[2:4],function(x) sum(.$time<= x))))
 
 
 
