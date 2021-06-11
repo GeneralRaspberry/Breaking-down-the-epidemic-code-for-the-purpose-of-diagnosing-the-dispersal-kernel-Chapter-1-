79ec9955338a250c1f89afa0eb1a09ee58142267
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-11 15:33:41 +0100

    Add files via upload
    Label on plot

--- experimental blueprint for the challenge of creating a parameter space by 17th June.R
+++ experimental blueprint for the challenge of creating a parameter space by 17th June.R
@@ -272,7 +272,9 @@ temptimemax<-temptimemax[,"time"]
 pred_data <- data.frame(time=times, infected=logis(r=mean_r, t=times, K=1000, q0=1))
 ggplot(temp) + geom_line(aes(x=time, y=infected, group=sim), size=.2) +
   geom_line(data=filter(pred_data, infected<1000), aes(x=time, y=infected), colour="red", size=1)+
-  ggtitle("Epidemic growth curve for 1000 simulations")+theme_classic()+xlim(0,temptimemax)+annotate(sprintf("%f days until .25 prevalence", days),x=100,y=100)
+  ggtitle("Epidemic growth curve for 1000 simulations")+theme_classic()+xlim(0,temptimemax)+annotate(geom="text", 
+                                                                                                     label=sprintf("%f days until .25 prevalence", days),
+                                                                                                     x=100,y=100)
 length(unique(unlist(par_r)))
 mean_r
 proc.end2<-proc.time()-t2
